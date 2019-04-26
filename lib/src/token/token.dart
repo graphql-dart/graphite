1c363cc40ef205ac46b4fce4d4d1ebcc2dd8da06
Roman Vanesyan <romain.vanesyan@gmail.com>
2019-04-26 23:29:21 +0500

    Update `TokenKind` class, so now can be used in `switch` statements.

--- lib/src/token/token.dart
+++ lib/src/token/token.dart
@@ -8,79 +8,76 @@
 import 'package:graphite_language/token.dart' show Spanning;
 
 class TokenKind {
-  const TokenKind._(this._value, this._name);
+  const TokenKind._(this._name);
 
-  final int _value;
   final String _name;
-
-  @override
-  int get hashCode => _value;
-
-  @override
-  bool operator ==(Object other) =>
-      other is TokenKind && other._value == _value;
 
   @override
   String toString() => 'TokenKind(value=$_name)';
 
-  static const TokenKind eof = TokenKind._(-1, '<EOF>');
-  static const TokenKind comment = TokenKind._(0, '<COMMENT>');
+  static const TokenKind eof = TokenKind._('<EOF>');
+  static const TokenKind comment = TokenKind._('<COMMENT>');
 
-  static const TokenKind blockStringValue =
-      TokenKind._(1, '<BLOCK_STRING_VALUE>');
-  static const TokenKind booleanValue = TokenKind._(2, '<BOOLEAN_VALUE>');
-  static const TokenKind floatValue = TokenKind._(3, '<FLOAT_VALUE>');
-  static const TokenKind integerValue = TokenKind._(4, '<INTEGER_VALUE>');
-  static const TokenKind stringValue = TokenKind._(4, '<STRING_VALUE>');
+  static const TokenKind blockStringValue = TokenKind._('<BLOCK_STRING_VALUE>');
+  static const TokenKind booleanValue = TokenKind._('<BOOLEAN_VALUE>');
+  static const TokenKind floatValue = TokenKind._('<FLOAT_VALUE>');
+  static const TokenKind integerValue = TokenKind._('<INTEGER_VALUE>');
+  static const TokenKind stringValue = TokenKind._('<STRING_VALUE>');
 
-  static const TokenKind bang = TokenKind._(100, '!');
-  static const TokenKind dollar = TokenKind._(101, '\$');
-  static const TokenKind parenl = TokenKind._(102, '(');
-  static const TokenKind parenr = TokenKind._(103, ')');
-  static const TokenKind spread = TokenKind._(104, '...');
-  static const TokenKind colon = TokenKind._(105, ':');
-  static const TokenKind eq = TokenKind._(106, '=');
-  static const TokenKind at = TokenKind._(107, '@');
-  static const TokenKind bracel = TokenKind._(108, '[');
-  static const TokenKind bracer = TokenKind._(109, ']');
-  static const TokenKind bracketl = TokenKind._(110, '{');
-  static const TokenKind pipe = TokenKind._(111, '|');
-  static const TokenKind bracketr = TokenKind._(112, '}');
+  static const TokenKind bang = TokenKind._('!');
+  static const TokenKind dollar = TokenKind._('\$');
+  static const TokenKind parenl = TokenKind._('(');
+  static const TokenKind parenr = TokenKind._(')');
+  static const TokenKind spread = TokenKind._('...');
+  static const TokenKind colon = TokenKind._(':');
+  static const TokenKind eq = TokenKind._('=');
+  static const TokenKind at = TokenKind._('@');
+  static const TokenKind bracel = TokenKind._('[');
+  static const TokenKind bracer = TokenKind._(']');
+  static const TokenKind bracketl = TokenKind._('{');
+  static const TokenKind pipe = TokenKind._('|');
+  static const TokenKind bracketr = TokenKind._('}');
 
-  static const TokenKind ident = TokenKind._(200, '<IDENTIFIER>');
-  static const TokenKind enumKeyword = TokenKind._(201, 'enum');
-  static const TokenKind extendKeyword = TokenKind._(202, 'extend');
-  static const TokenKind fragmentKeyword = TokenKind._(203, 'fragment');
-  static const TokenKind implementsKeyword = TokenKind._(204, 'implements');
-  static const TokenKind inputKeyword = TokenKind._(205, 'input');
-  static const TokenKind interfaceKeyword = TokenKind._(206, 'interface');
-  static const TokenKind mutationKeyword = TokenKind._(207, 'mutation');
-  static const TokenKind nullKeyword = TokenKind._(208, 'null');
-  static const TokenKind onKeyword = TokenKind._(209, 'on');
-  static const TokenKind queryKeyword = TokenKind._(210, 'query');
-  static const TokenKind scalarKeyword = TokenKind._(211, 'scalar');
-  static const TokenKind schemaKeyword = TokenKind._(212, 'schema');
-  static const TokenKind subscriptionKeyword = TokenKind._(213, 'subscription');
-  static const TokenKind typeKeyword = TokenKind._(214, 'type');
-  static const TokenKind unionKeyword = TokenKind._(215, 'union');
+  static const TokenKind ident = TokenKind._('<IDENTIFIER>');
+  static const TokenKind enumKeyword = TokenKind._('enum');
+  static const TokenKind extendKeyword = TokenKind._('extend');
+  static const TokenKind fragmentKeyword = TokenKind._('fragment');
+  static const TokenKind implementsKeyword = TokenKind._('implements');
+  static const TokenKind inputKeyword = TokenKind._('input');
+  static const TokenKind interfaceKeyword = TokenKind._('interface');
+  static const TokenKind mutationKeyword = TokenKind._('mutation');
+  static const TokenKind nullKeyword = TokenKind._('null');
+  static const TokenKind onKeyword = TokenKind._('on');
+  static const TokenKind queryKeyword = TokenKind._('query');
+  static const TokenKind scalarKeyword = TokenKind._('scalar');
+  static const TokenKind schemaKeyword = TokenKind._('schema');
+  static const TokenKind subscriptionKeyword = TokenKind._('subscription');
+  static const TokenKind typeKeyword = TokenKind._('type');
+  static const TokenKind unionKeyword = TokenKind._('union');
 
   /// Tests whether [kind] is some keyword kind.
-  static bool isKeyword(TokenKind kind) =>
-      kind == enumKeyword ||
-      kind == extendKeyword ||
-      kind == fragmentKeyword ||
-      kind == implementsKeyword ||
-      kind == inputKeyword ||
-      kind == interfaceKeyword ||
-      kind == mutationKeyword ||
-      kind == nullKeyword ||
-      kind == onKeyword ||
-      kind == queryKeyword ||
-      kind == scalarKeyword ||
-      kind == schemaKeyword ||
-      kind == subscriptionKeyword ||
-      kind == typeKeyword ||
-      kind == unionKeyword;
+  static bool isKeyword(TokenKind kind) {
+    switch (kind) {
+      case enumKeyword:
+      case extendKeyword:
+      case fragmentKeyword:
+      case implementsKeyword:
+      case inputKeyword:
+      case interfaceKeyword:
+      case mutationKeyword:
+      case nullKeyword:
+      case onKeyword:
+      case queryKeyword:
+      case scalarKeyword:
+      case schemaKeyword:
+      case subscriptionKeyword:
+      case typeKeyword:
+      case unionKeyword:
+        return true;
+    }
+
+    return false;
+  }
 }
 
 class Token {
