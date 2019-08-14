b3ad6cdc550fd477cd1ad1d57dd64d889b55d4c3
Roman Vanesyan <romain.vanesyan@gmail.com>
2019-08-14 13:37:07 +0500

    Implement InterfaceTypeDefinition parsing

--- lib/src/ast/interface_type_definition.dart
+++ lib/src/ast/interface_type_definition.dart
@@ -9,13 +9,23 @@ part of graphite.language.ast;
 
 /// https://graphql.github.io/graphql-spec/draft/#InterfaceTypeDefinition
 class InterfaceTypeDefinition extends Definition {
-  const InterfaceTypeDefinition();
+  const InterfaceTypeDefinition(
+      {@required this.name, this.description, this.directives, this.fields});
+
+  final String name;
+  final String description;
+  final Iterable<Directive> directives;
+  final Iterable<FieldDefinition> fields;
 
   @override
   NodeKind get kind => NodeKind.interfaceTypeDefinition;
 
   @override
   Map<String, Object> toJson() => {
-    'kind': kind.toString(),
-  };
+        'kind': kind.toString(),
+        'name': name,
+        'description': description,
+        'directives': directives,
+        'fields': fields,
+      };
 }
