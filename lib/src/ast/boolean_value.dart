4d85cb66cbff29c0283801a5ac9e7ea2e9018b9a
Roman Vanesyan <romain.vanesyan@gmail.com>
2019-08-16 01:21:04 +0500

    Disable false-positive linting

--- lib/src/ast/boolean_value.dart
+++ lib/src/ast/boolean_value.dart
@@ -9,6 +9,7 @@ part of graphite.language.ast;
 
 /// https://graphql.github.io/graphql-spec/draft/#BooleanValue
 class BooleanValue extends Value<bool> {
+  // ignore:avoid_positional_boolean_parameters
   const BooleanValue(bool value) : super(value);
 
   @override
