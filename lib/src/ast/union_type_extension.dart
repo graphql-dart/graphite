91e6d5c7979566898ac08cb91a30772631455340
Roman Vanesyan <romain.vanesyan@gmail.com>
2019-08-18 13:47:51 +0500

    Add UnionTypeExtension AST node

---
+++ lib/src/ast/union_type_extension.dart
@@ -0,0 +1,29 @@
+// Copyright (c) The Graphite project authors. See AUTHORS file for details.
+// All rights reserved.
+//
+// Use of this source code is governed under the BSD-3-Clause license
+// which can be found in the LICENSE file in the root directory of
+// this source tree.
+
+part of graphite.language.ast;
+
+/// https://graphql.github.io/graphql-spec/draft/#UnionTypeExtension
+class UnionTypeExtension extends Extension {
+  const UnionTypeExtension(
+      {@required this.name, this.directives, this.members});
+
+  final String name;
+  final Iterable<Directive> directives;
+  final Iterable<NamedType> members;
+
+  @override
+  NodeKind get kind => NodeKind.unionTypeExtension;
+
+  @override
+  Map<String, Object> toJson() => {
+        'kind': kind.toString(),
+        'name': name,
+        'directives': directives,
+        'members': members,
+      };
+}
